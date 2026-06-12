4e47f0f808ccaaceefda803b87d93f5a41d623f1
Ryan El Kochta <relkochta@google.com>
2026-06-12 09:46:09 -0700

    Make /proc/sys/kernel/hostname and /proc/sys/kernel/domainname pollable
    Adds the infrastructure for a DynamicBytesFile to become pollable
    (perhaps with select, poll, or epoll). Simply arrange for a
    DynamicBytesPoller to be notified whenever the relevant file is updated
    and implement PollableDynamicBytesSource on the DynamicBytesSource.
    
    EPOLLPRI and EPOLLERR are triggered for these files, which seems to
    match Linux's implementation for /proc/sys files.
    
    The first two files to support this are hostname and domainname in
    /proc/sys/kernel. This is used by systemd-journald to monitor for
    hostname changes.

--- pkg/sentry/fsimpl/proc/tasks_sys.go
+++ pkg/sentry/fsimpl/proc/tasks_sys.go
@@ -199,6 +199,11 @@ func (*hostnameData) Generate(ctx context.Context, buf *bytes.Buffer) error {
 	return nil
 }
 
+// GetDynamicBytesPoller implements vfs.PollableDynamicBytesSource.GetDynamicBytesPoller.
+func (*hostnameData) GetDynamicBytesPoller(ctx context.Context) *vfs.DynamicBytesPoller {
+	return &kernel.KernelFromContext(ctx).HostNamePoller
+}
+
 // domainnameData implements vfs.DynamicBytesSource for /proc/sys/kernel/domainname.
 //
 // +stateify savable
@@ -215,6 +220,11 @@ func (*domainnameData) Generate(ctx context.Context, buf *bytes.Buffer) error {
 	buf.WriteString(utsns.DomainName())
 	buf.WriteString("\n")
 	return nil
+}
+
+// GetDynamicBytesPoller implements vfs.PollableDynamicBytesSource.GetDynamicBytesPoller.
+func (*domainnameData) GetDynamicBytesPoller(ctx context.Context) *vfs.DynamicBytesPoller {
+	return &kernel.KernelFromContext(ctx).DomainNamePoller
 }
 
 // tcpSackData implements vfs.WritableDynamicBytesSource for
